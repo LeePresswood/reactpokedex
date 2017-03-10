aa1cfe3cbe58da7fdc5a0441c1964d28fb5bfc5f
Lee Presswood <LeePresswood@gmail.com>
2017-03-10 01:31:46 -0600

    Removed unneeded binary converter class.

--- src/main/java/nn/common/util/BinaryConverter.java
+++
@@ -1,9 +0,0 @@
-package nn.common.util;
-
-import java.math.BigInteger;
-
-public class BinaryConverter {
-   public static String toBinaryArray(byte[] bytes) {
-      return new BigInteger(bytes).toString();
-   }
-}
