6fbeb9a134500eff09ba028da70a8bc50ee70d97
Lee Presswood <LeePresswood@gmail.com>
2017-03-11 19:07:09 -0600

    Using a BufferedReader instead.

--- src/main/java/com/leepresswood/common/util/ImageUtility.java
+++ src/main/java/com/leepresswood/common/util/ImageUtility.java
@@ -1,24 +1,32 @@
 package com.leepresswood.common.util;
 
+import com.leepresswood.common.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.PixelReader;
-import com.leepresswood.common.Slider;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 public class ImageUtility {
-   public static Image readImage(String path){
-      return new Image(new File(path).toURI().toString());
+   public static BufferedImage readImage(String path) {
+      try {
+         return ImageIO.read(new File(path));
+      } catch(IOException e) {
+         e.printStackTrace();
+         return null;
+      }
+   
    }
    
-   public static Slider[] imageToSliderArray(Image image){
+   public static Slider[] imageToSliderArray(BufferedImage image) {
       int width = (int) image.getWidth();
       int height = (int) image.getHeight();
-      PixelReader reader = image.getPixelReader();
       
       Slider[] sliders = new Slider[width * height];
       int current = 0;
-   
+      
       for(int i = 0; i < width; i++) {
          for(int j = 0; j < height; j++) {
             sliders[current++] = new Slider(reader, i, j);
