59148912e25edfcfccd5d47f814768466e731aed
Lee Presswood <LeePresswood@gmail.com>
2017-03-14 00:57:45 -0500

    Added combined binary arrays method.

--- src/main/java/com/leepresswood/common/domain/InputPlug.java
+++ src/main/java/com/leepresswood/common/domain/InputPlug.java
@@ -1,9 +1,20 @@
 package com.leepresswood.common.domain;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 /**
  * Contains the inputs for the neural network.
  * Use this to manage order and value of input.
  */
 public class InputPlug {
+   private boolean[] slider;
    
+   
+   public void setSlider(boolean[] slider) {
+      this.slider = slider;
+   }
+   
+   public boolean[] getCombinedBinaryInput(){
+      return ArrayUtils.addAll(slider);
+   }
 }
