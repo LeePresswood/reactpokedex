f70b072779f94a3cc7292ea9ed7574b55ca63ddc
Lee Presswood <LeePresswood@gmail.com>
2017-06-30 22:05:05 -0500

    Added basic Header and Content components

---
+++ src/components/content/Content.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Content.css';
+
+class Content extends Component {
+    render() {
+        return (
+            <div className="Content">
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Content;
