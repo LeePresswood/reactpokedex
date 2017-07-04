99e1c6c22a4d539d76153b746d81afa4f4e5373d
Lee Presswood <LeePresswood@gmail.com>
2017-07-04 16:55:04 -0500

    Added Project component.

---
+++ src/components/project/Project.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+// import './App.css';
+
+class Project extends Component {
+    render(){
+        return (
+            <div className="Project">
+                    <p>
+                        <b>Title: </b> {this.props.project.title}
+                    </p>
+                    <p>
+                        <b>Description: </b> {this.props.project.description}
+                    </p>
+                    <p>
+                        <b>Tags: </b> {this.props.project.tags.join(" | ")}
+                    </p>
+            </div>
+        );
+    }
+}
+
+export default Project;
