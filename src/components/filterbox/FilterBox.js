d57f7e140b0a40786ae337ec642033102ca17628
Lee Presswood <LeePresswood@gmail.com>
2017-07-04 17:07:01 -0500

    Added mock filter data

--- src/components/filterbox/FilterBox.js
+++ src/components/filterbox/FilterBox.js
@@ -1,16 +1,40 @@
 import React, {Component} from "react";
+import Filter from "../filter/Filter";
 // import './App.css';
 
 class FilterBox extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            filters : [
+                {
+                    filterId: 1,
+                    filter : "java",
+                    projectCount : 100
+                },
+                {
+                    filterId: 2,
+                    filter : "javascript",
+                    projectCount : 1000
+                }
+            ]
+        }
+    }
+    
+    renderFilters(){
+        return this.state.filters.map(filter => <Filter key={filter.filterId} filter={filter}/>);
+    }
+    
+    setFilter(filter){
+        alert("Filter: " + filter);
+    }
+    
     render(){
         return (
             <div className="FilterBox">
                 <h4>Filters</h4>
-                <p><a href="#">Java</a> (100)</p>
-                <p><a href="#">Javascript</a> (100)</p>
-                <p><a href="#">C#</a> (100)</p>
-                <p><a href="#">C++</a> (100)</p>
-                <p><a href="#">C</a> (100)</p>
+                {this.renderFilters()}
             </div>
         );
     }
--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-// import './App.css';
+import './Project.css';
 
 class Project extends Component {
     render(){
