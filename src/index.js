532e2d9af902f024a6aead5c0ad9d266d61dfbc5
Lee Presswood <LeePresswood@gmail.com>
2017-07-08 13:34:34 -0500

    Added project.

--- src/index.js
+++ src/index.js
@@ -1,66 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {Provider} from "react-redux";
-import {createStore} from "redux";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import Reducers from "./reducers/Reducers";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-const initialState = {
-    selectedFilters : []
-};
-
-const store = createStore(Reducers, initialState);
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-//
-// const reducer = (state = {
-//     result : 1,
-//     lastValues : []
-// }, action) =>{
-//     switch(action.type){
-//         case "ADD":
-//             state = {
-//                 ...state,
-//                 result : state.result + action.payload,
-//                 lastValues : [...state.lastValues, action.payload]
-//             };
-//             break;
-//         case "SUBTRACT":
-//             state = {
-//                 ...state,
-//                 result : state.result - action.payload,
-//                 lastValues : [...state.lastValues, action.payload]
-//             };
-//             break;
-//     }
-//
-//     return state;
-// };
-//
-// const store = createStore(reducer);
-//
-// store.subscribe(() =>{
-//     console.log("Store updated!", store.getState())
-// });
-//
-// store.dispatch({
-//     type : "ADD",
-//     payload : 100
-// });
-//
-// store.dispatch({
-//     type : "ADD",
-//     payload : 22
-// });
-//
-// store.dispatch({
-//     type : "SUBTRACT",
-//     payload : 80
-// });
