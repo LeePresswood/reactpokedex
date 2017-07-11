8c02429b784358429e32b3ec263bf6fcf6ce2642
Lee Presswood <LeePresswood@gmail.com>
2017-07-11 01:34:28 -0500

    Added dynamic number of boxes.

--- src/components/ContentSection.js
+++ src/components/ContentSection.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import '../styles/ContentSection.css';
+import React, {Component} from "react";
+import "../styles/ContentSection.css";
 import PokeRows from "./PokeRows";
 import PokeFilter from "./PokeFilter";
 
 class ContentSection extends Component {
-    render() {
+    render(){
         return (
             <div className="ContentSection">
                 <PokeFilter/>
-                <PokeRows/>
+                <PokeRows pokeArray={new Array(10).fill(0).map((_, i) => i + 1)}/>
             </div>
         );
     }
--- src/components/PokeRows.js
+++ src/components/PokeRows.js
@@ -5,13 +5,9 @@ class PokeRows extends Component {
     render() {
         return (
             <div className="PokeRows">
-                <div className="box">A</div>
-                <div className="box">B</div>
-                <div className="box">C</div>
-                <div className="box">D</div>
-                <div className="box">E</div>
-                <div className="box">F</div>
-                <div className="box">G</div>
+                {this.props.pokeArray.map((pokemon) =>
+                    <div key={pokemon} className="box">{pokemon}</div>
+                )}
             </div>
         );
     }
