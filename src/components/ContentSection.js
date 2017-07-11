7e526eb6a402d5f4ad6db8ca0daa5cd28c6f5d31
Lee Presswood <LeePresswood@gmail.com>
2017-07-10 23:37:34 -0500

    Added PokeFilter section.

--- src/components/ContentSection.js
+++ src/components/ContentSection.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import '../styles/ContentSection.css';
 import PokeRows from "./PokeRows";
+import PokeFilter from "./PokeFilter";
 
 class ContentSection extends Component {
     render() {
         return (
             <div className="ContentSection">
+                <PokeFilter/>
                 <PokeRows/>
             </div>
         );
---
+++ src/components/PokeFilter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import '../styles/ContentSection.css';
+
+class PokeFilter extends Component {
+    render() {
+        return (
+            <div className="PokeFilter">
+                <h1>Filter Here</h1>
+            </div>
+        );
+    }
+}
+
+export default PokeFilter;
