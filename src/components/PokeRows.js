ab53a14c1466c03804853923e3c0d0532235f585
Lee Presswood <LeePresswood@gmail.com>
2017-07-12 01:10:41 -0500

    Read Pokemon list from mapped file.

--- src/components/PokeRows.js
+++ src/components/PokeRows.js
@@ -1,28 +1,11 @@
 import React, {Component} from "react";
 import Pokemon from "./Pokemon";
 import "../styles/PokeRows.css";
+import pokemonList from "../pokemonMapped.json"
 
 class PokeRows extends Component {
     mapFilteredPokemonToComponents(){
-        let pokeNames = [
-            "Bulbasaur",
-            "Ivysaur",
-            "Venusaur",
-            "Charmander",
-            "Charmeleon",
-            "Charizard",
-            "Squirtle",
-            "Wartortle",
-            "Blastoise",
-            "Caterpie",
-        ]
-        
-        return this.props.pokeArray.map((_, i) =>{
-            return {
-                number: i + 1,
-                name: pokeNames[i]
-            }
-        }).map((pokemon) =>
+        return pokemonList.map((pokemon) =>
             <Pokemon key={pokemon.number} {...pokemon} />
         );
     }
