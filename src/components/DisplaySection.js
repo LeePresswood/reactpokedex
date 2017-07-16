6e621c171d148123c70378bbcbb298e0f286170a
Lee Presswood <LeePresswood@gmail.com>
2017-07-15 22:39:02 -0500

    Trimmed the fat.

---
+++ src/components/DisplaySection.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+// import "../styles/ContentSection.css";
+// import PokeRows from "./PokeRows";
+// import PokeFilter from "./PokeFilter";
+
+class DisplaySection extends Component {
+    render(){
+        return (
+            <div className="DisplaySection">
+                {/*<PokeFilter />*/}
+                {/*<PokeRows pokeArray={new Array(10).fill({})}/>*/}
+            </div>
+        );
+    }
+}
+
+export default DisplaySection;
