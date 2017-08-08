edd1fb516b63b24e460222067bb2274d8e85b036
Lee Presswood <LeePresswood@gmail.com>
2017-08-08 17:47:26 -0500

    Hiding image styling for now.

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,9 +2,21 @@ import React, {Component} from "react";
 import "../styles/Pokemon.css";
 
 class Pokemon extends Component {
+    getBackgroundPosition(){
+        return "-360px -120px";
+    }
+    
+    getStyle(){
+        return {
+            // backgroundSize: '40px 40px',
+            // backgroundRepeat: 'no-repeat',
+            // backgroundPosition: this.getBackgroundPosition()
+        };
+    }
+    
     render(){
         return (
-            <div className="Pokemon col-sm-4">
+            <div className="Pokemon col-sm-4" style={this.getStyle()}>
                 <div>{this.props.number}</div>
                 <div>{this.props.name}</div>
                 <div>Picture</div>
